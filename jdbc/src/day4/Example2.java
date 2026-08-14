7611ee5380f8fd606d6975096b4ddc9c33b9098f
Sumanjali8324 <sumanjali8324@gmail.com>
2026-08-14 16:04:35 +0530

    patterns

---
+++ jdbc/src/day4/Example2.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=4;
+		int c=1;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(c+" ");
+				c++;
+			}System.out.println();
+		}
+
+	}
+
+}
