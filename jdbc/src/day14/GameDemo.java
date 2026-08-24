21648837eb2798692cc072d7c954f2f6cea9b25e
Sumanjali8324 <sumanjali8324@gmail.com>
2026-08-24 23:18:24 +0530

    problems

--- jdbc/src/day14/GameDemo.java
+++ jdbc/src/day14/GameDemo.java
@@ -1,5 +1,121 @@
 package day14;
+abstract class Character {
+
+    private String name;
+    private int health;
+    private int power;
+
+    Character(String name, int health, int power) {
+        this.name = name;
+        this.health = health;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void reduceHealth(int damage) {
+
+        health -= damage;
+
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    abstract void attack();
+
+    abstract void defend();
+}
+
+class Warrior extends Character {
+
+    Warrior(String name, int health, int power) {
+        super(name, health, power);
+    }
+
+    @Override
+    void attack() {
+        System.out.println(getName() + " attacks with Sword");
+    }
+
+    @Override
+    void defend() {
+        System.out.println(getName() + " defends with Shield");
+    }
+}
+
+class Mage extends Character {
+
+    Mage(String name, int health, int power) {
+        super(name, health, power);
+    }
+
+    @Override
+    void attack() {
+        System.out.println(getName() + " attacks with Magic");
+    }
+
+    @Override
+    void defend() {
+        System.out.println(getName() + " defends with Magic Barrier");
+    }
+}
+
+class Archer extends Character {
+
+    Archer(String name, int health, int power) {
+        super(name, health, power);
+    }
+
+    @Override
+    void attack() {
+        System.out.println(getName() + " attacks with Bow");
+    }
+
+    @Override
+    void defend() {
+        System.out.println(getName() + " defends by Dodging");
+    }
+}
 
 public class GameDemo {
 
+    public static void main(String[] args) {
+
+        Character c1 =
+            new Warrior("Warrior", 100, 30);
+
+        Character c2 =
+            new Mage("Mage", 80, 40);
+
+        Character c3 =
+            new Archer("Archer", 90, 25);
+
+        c1.attack();
+        c1.defend();
+
+        c2.attack();
+        c2.defend();
+
+        c3.attack();
+        c3.defend();
+
+        c1.reduceHealth(40);
+
+        System.out.println("Warrior Health: " + c1.getHealth());
+
+        c1.reduceHealth(100);
+
+        System.out.println("Warrior Health: " + c1.getHealth());
+    }
 }
--- jdbc/src/day14/LibraryDemo.java
+++ jdbc/src/day14/LibraryDemo.java
@@ -1,5 +1,150 @@
 package day14;
+import java.util.ArrayList;
+
+class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean available = true;
+
+    Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void issue() {
+        available = false;
+    }
+
+    public void returnBook() {
+        available = true;
+    }
+
+    public void display() {
+        System.out.println(id + " " + title + " " + author);
+    }
+}
+
+class Member {
+
+    private int id;
+    private String name;
+
+    Member(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Library {
+
+    ArrayList<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void searchBook(String title) {
+
+        for (Book book : books) {
+
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                book.display();
+                return;
+            }
+        }
+
+        System.out.println("Book not found");
+    }
+
+    public void issueBook(String title, Member member) {
+
+        for (Book book : books) {
+
+            if (book.getTitle().equalsIgnoreCase(title)) {
+
+                if (book.isAvailable()) {
+                    book.issue();
+                    System.out.println(
+                        title + " issued to " + member.getName()
+                    );
+                } else {
+                    System.out.println("Book already issued");
+                }
+
+                return;
+            }
+        }
+
+        System.out.println("Book not found");
+    }
+
+    public void returnBook(String title) {
+
+        for (Book book : books) {
+
+            if (book.getTitle().equalsIgnoreCase(title)) {
+
+                book.returnBook();
+                System.out.println("Book returned");
+                return;
+            }
+        }
+
+        System.out.println("Book not found");
+    }
+
+    public void displayBooks() {
+
+        for (Book book : books) {
+
+            if (book.isAvailable()) {
+                book.display();
+            }
+        }
+    }
+}
 
 public class LibraryDemo {
 
+    public static void main(String[] args) {
+
+        Library library = new Library();
+
+        Book b1 = new Book(101, "Java", "James");
+        Book b2 = new Book(102, "SQL", "John");
+        Book b3 = new Book(103, "Spring", "Craig");
+
+        library.addBook(b1);
+        library.addBook(b2);
+        library.addBook(b3);
+
+        Member m1 = new Member(1, "Suman");
+
+        library.searchBook("Java");
+
+        library.issueBook("Java", m1);
+
+        library.issueBook("Java", m1);
+
+        library.displayBooks();
+
+        library.returnBook("Java");
+
+        library.displayBooks();
+    }
 }
